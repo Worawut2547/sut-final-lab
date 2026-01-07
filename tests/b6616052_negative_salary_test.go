f1d50f49bea9c4815a0110f930a37ab637063b29
Worawut2547 <Wutsab@gmail.com>
2026-01-07 15:08:43 +0700

    valid data collected - close #2

---
+++ tests/b6616052_negative_salary_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"testing"
+	"fmt"
+
+	"sut-final-lab/entities"
+	."github.com/onsi/gomega"
+)
+
+func TestSalaryEmployee(t *testing.T) {
+	empInfo := entities.Employee{
+		Name: "Worawut",
+		Salary: 20000,
+		EmployeeCode: "EMP6616",
+	}
+
+	fmt.Println("Case 2: Salary must be between 15000 and 200000")
+	t.Run("Salary must be between 15000 and 200000", func(t *testing.T){
+		g := NewGomegaWithT(t)
+		obj := empInfo
+		obj.Salary = 150
+		err := obj.Validation()
+
+		g.Expect(err).ToNot(BeNil())
+	})
+}
